54f5c6cb82d409c974f533c90b99671217e7fe30
Christopher <christopher.encarnacion@gmail.com>
2017-08-08 20:37:31 -0400

    modified the script and made the css responsive.

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
 var screen = document.getElementById("screen");
     
-function input(value) {
-  var theValue = document.getElementById("screen");
-  switch (value) {
-    case "+":
-      if(screen.value[screen.value.length - 1] == "+") {
-        break;
-      } else
-        theValue.value += "+"
-      break;
-      case "*":
-      if(screen.value[screen.value.length - 1] == "*") {
-        break;
-      } else
-        theValue.value += "*"
-      break;
-      case "/":
-      if(screen.value[screen.value.length - 1] == "/") {
-        break;
-      } else
-        theValue.value += "/"
-      break;
-    
-    default:    
-  theValue.value += value;
-      break;
+function input(value) {  
+  if(!parseInt(value) && screen.value.length > 0 && parseInt(screen.value[screen.value.length -1])) {
+    screen.value += value;
+  } else if (parseInt(value)) {
+    screen.value += value;
   }
 }
 
 function calculate() {
   var total = eval(screen.value);
   screen.value = total;
+}
+
+function wipe() {
+  screen.value ="";
 }
